b08691ad059f93eafa11f0ae8bb387d978d8d659
Michael Ernst <mernst@cs.washington.edu>
2016-01-20 12:06:18 -0800

    Reinstate doing work itself

--- .travis-build.sh
+++ .travis-build.sh
@@ -4,19 +4,20 @@ ROOT=$TRAVIS_BUILD_DIR/..
 # Fail the whole script if any command fails
 set -e
 
-echo "Should next trigger downstream jobs."
+## Short version, intended to be used when triggering downstream Travis jobs.
+# echo "Should next trigger downstream jobs."
+# true
 
-true
-
+## Long version, that does all the work itself.
 ## Problem: this times out.  I need to break it into different Travis jobs.
 ## So just always succeed and run the other jobs.
-# ## Build Checker Framework
-# (cd $ROOT && git clone https://github.com/typetools/checker-framework.git)
-# # This also builds annotation-tools and jsr308-langtools
-# (cd $ROOT/checker-framework/ && ./.travis-build-without-test.sh)
-# export CHECKERFRAMEWORK=$ROOT/checker-framework
-# 
-# ## Obtain daikon
-# (cd $ROOT && git clone https://github.com/codespecs/daikon.git)
-# 
-# make -C $ROOT/daikon/java check-all
+## Build Checker Framework
+(cd $ROOT && git clone https://github.com/typetools/checker-framework.git)
+# This also builds annotation-tools and jsr308-langtools
+(cd $ROOT/checker-framework/ && ./.travis-build-without-test.sh)
+export CHECKERFRAMEWORK=$ROOT/checker-framework
+
+## Obtain daikon
+(cd $ROOT && git clone https://github.com/codespecs/daikon.git)
+
+make -C $ROOT/daikon/java check-all
