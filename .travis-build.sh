12abdacbba1e0baacee198ccfdfa61872717922b
Michael Ernst <mernst@cs.washington.edu>
2016-01-20 12:42:40 -0800

    Experimentally fail the build

--- .travis-build.sh
+++ .travis-build.sh
@@ -7,6 +7,8 @@ set -e
 ## Short version, intended to be used when triggering downstream Travis jobs.
 echo "Should next trigger downstream jobs."
 true
+# Experimentally fail the build
+false
 
 ## Long version, that does all the work itself.
 ## Problem: this times out.  I need to break it into different Travis jobs.
