ec0c0b2268da7c8ed13a8b21ff289aa26c461afb
Michael Ernst <mernst@cs.washington.edu>
2016-01-20 12:26:11 -0800

    Resume delegating work

--- .travis-build.sh
+++ .travis-build.sh
@@ -5,19 +5,19 @@ ROOT=$TRAVIS_BUILD_DIR/..
 set -e
 
 ## Short version, intended to be used when triggering downstream Travis jobs.
-# echo "Should next trigger downstream jobs."
-# true
+echo "Should next trigger downstream jobs."
+true
 
 ## Long version, that does all the work itself.
 ## Problem: this times out.  I need to break it into different Travis jobs.
 ## So just always succeed and run the other jobs.
-## Build Checker Framework
-(cd $ROOT && git clone https://github.com/typetools/checker-framework.git)
-# This also builds annotation-tools and jsr308-langtools
-(cd $ROOT/checker-framework/ && ./.travis-build-without-test.sh)
-export CHECKERFRAMEWORK=$ROOT/checker-framework
-
-## Obtain daikon
-(cd $ROOT && git clone https://github.com/codespecs/daikon.git)
-
-make -C $ROOT/daikon/java check-all
+# ## Build Checker Framework
+# (cd $ROOT && git clone https://github.com/typetools/checker-framework.git)
+# # This also builds annotation-tools and jsr308-langtools
+# (cd $ROOT/checker-framework/ && ./.travis-build-without-test.sh)
+# export CHECKERFRAMEWORK=$ROOT/checker-framework
+# 
+# ## Obtain daikon
+# (cd $ROOT && git clone https://github.com/codespecs/daikon.git)
+# 
+# make -C $ROOT/daikon/java check-all
