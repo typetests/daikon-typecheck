8e09a69a5fae9e3d54c2c935b04bab67fcaf0fb7
Michael Ernst <mernst@cs.washington.edu>
2016-05-08 08:45:28 -0700

    Reduce git clone depth

--- .travis-build.sh
+++ .travis-build.sh
@@ -12,12 +12,12 @@ true
 ## Problem: this times out.  I need to break it into different Travis jobs.
 ## So just always succeed and run the other jobs.
 # ## Build Checker Framework
-# (cd $ROOT && git clone https://github.com/typetools/checker-framework.git)
+# (cd $ROOT && git clone --depth 1 https://github.com/typetools/checker-framework.git)
 # # This also builds annotation-tools and jsr308-langtools
 # (cd $ROOT/checker-framework/ && ./.travis-build-without-test.sh)
 # export CHECKERFRAMEWORK=$ROOT/checker-framework
 # 
 # ## Obtain daikon
-# (cd $ROOT && git clone https://github.com/codespecs/daikon.git)
+# (cd $ROOT && git clone --depth 1 https://github.com/codespecs/daikon.git)
 # 
 # make -C $ROOT/daikon/java check-all
