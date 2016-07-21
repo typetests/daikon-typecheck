fc2078a5bc23c4c2da74385f9c718059965fa058
Michael Ernst <mernst@cs.washington.edu>
2016-07-21 05:27:30 -0700

    Make separate typecheckers be called as subjobs, not downstream builds

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 ROOT=$TRAVIS_BUILD_DIR/..
 
+# Required argument $1 is one of:
+#   formatter, interning, lock, nullness, regex, signature, nothing
+
+
 # Fail the whole script if any command fails
 set -e
 
@@ -8,16 +12,52 @@ set -e
 echo "Should next trigger downstream jobs."
 true
 
-## Long version, that does all the work itself.
-## Problem: this times out.  I need to break it into different Travis jobs.
-## So just always succeed and run the other jobs.
-# ## Build Checker Framework
-# (cd $ROOT && git clone --depth 1 https://github.com/typetools/checker-framework.git)
-# # This also builds annotation-tools and jsr308-langtools
-# (cd $ROOT/checker-framework/ && ./.travis-build-without-test.sh)
-# export CHECKERFRAMEWORK=$ROOT/checker-framework
-# 
-# ## Obtain daikon
-# (cd $ROOT && git clone --depth 1 https://github.com/codespecs/daikon.git)
-# 
-# make -C $ROOT/daikon/java check-all
+## Build Checker Framework
+(cd $ROOT && git clone --depth 1 https://github.com/typetools/checker-framework.git)
+# This also builds annotation-tools and jsr308-langtools
+(cd $ROOT/checker-framework/ && ./.travis-build-without-test.sh)
+export CHECKERFRAMEWORK=$ROOT/checker-framework
+ 
+## Obtain daikon
+(cd $ROOT && git clone --depth 1 https://github.com/codespecs/daikon.git)
+
+## Obtain daikon
+(cd $ROOT && git clone --depth 1 https://github.com/codespecs/daikon.git)
+## Is the dyncomp-jdk task needed?
+# make -C $ROOT/daikon/java compile dyncomp-jdk
+make -C $ROOT/daikon/java compile
+make -C $ROOT/daikon daikon.jar
+
+## It would be better to just run "make check-all", but that times out, so break
+## it into different Travis jobs.  That also helps to indicate the specific
+## error that occurred.
+## make -C $ROOT/daikon/java check-all
+
+# As of July 2016, timing for Travis jobs (which includes building the Checker Framework and Daikon):
+# check-formatter : 30 minutes
+# check-interning : 32 minutes
+# check-lock : 40 minutes
+# check-nullness : 96 minutes
+# check-regex : 33 minutes
+# check-signature : 30 minutes
+# How are the long run times for check-nullness possible?
+# Keep that as a downstream job if it times out now?
+
+if [[ "$1" == "formatter" ]]; then
+  make -C $ROOT/daikon/java check-formatter
+elif [[ "$1" == "interning" ]]; then
+  make -C $ROOT/daikon/java check-interning
+elif [[ "$1" == "lock" ]]; then
+  make -C $ROOT/daikon/java check-lock
+elif [[ "$1" == "nullness" ]]; then
+  make -C $ROOT/daikon/java check-nullness
+elif [[ "$1" == "regex" ]]; then
+  make -C $ROOT/daikon/java check-regex
+elif [[ "$1" == "signature" ]]; then
+  make -C $ROOT/daikon/java check-signature
+elif [[ "$1" == "nothing" ]]; then
+  true
+else
+  echo "Bad argument '$1' to travis-build.sh"
+  false
+fi
